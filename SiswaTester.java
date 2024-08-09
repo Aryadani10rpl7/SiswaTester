7e3f0adad90647edba6fd848345ada3c9cf5816a
Aryadani10rpl7 <aryadani_santoso_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:02:12 +0700

    Add files via upload

---
+++ SiswaTester.java
@@ -0,0 +1,56 @@
+// Driver Class
+public class SiswaTester {
+    public static void main(String[] args) {
+        // object
+        // Class object = new Constructor
+        Siswa arjuna = new Siswa();
+        Siswa matt = new Siswa();
+        Siswa evan = new Siswa();
+        Siswa rehan = new Siswa();
+        Siswa ryuichi = new Siswa();
+
+        matt.id = 20;
+        matt.nama = "Matthew";
+        matt.ipk = 90;
+
+        System.out.println(matt.id);
+        System.out.println(matt.nama);
+        System.out.println(matt.ipk);
+
+        arjuna.id = 1;
+        arjuna.nama = "Arjuna";
+        arjuna.ipk = 90;
+
+        System.out.println(arjuna.id);
+        System.out.println(arjuna.nama);
+        System.out.println(arjuna.ipk);
+
+        evan.id = 10;
+        evan.nama = "Evan";
+        evan.ipk = 90;
+
+        System.out.println(evan.id);
+        System.out.println(evan.nama);
+        System.out.println(evan.ipk);
+
+        rehan.id = 11;
+        rehan.nama = "rehan";
+        rehan.ipk = 90;
+
+        System.out.println(rehan.id);
+        System.out.println(rehan.nama);
+        System.out.println(rehan.ipk);
+
+        ryuichi.id = 32;
+        ryuichi.nama = "Ryu";
+        ryuichi.ipk = 90;
+
+        System.out.println(ryuichi.id);
+        System.out.println(ryuichi.nama);
+        System.out.println(ryuichi.ipk);
+
+
+
+
+    }
+}
